624deb07b096d0edfb7388c5fccd19b59dfd1dac
tzachs <tzach@catonetworks.com>
2018-10-08 15:12:47 +0300

    UI complete, need to check FormatException: Invalid double

--- lib/main.dart
+++ lib/main.dart
@@ -3,107 +3,183 @@ import 'package:flutter/material.dart';
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final appTitle = 'Shopping Helper';
+
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        title: appTitle,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text(appTitle),
+          ),
+          body: CompareForm(),
+        ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class CompareForm extends StatefulWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  _CompareForm createState() => new _CompareForm();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CompareForm extends State<CompareForm> {
+  final _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  final nameA = TextEditingController(text: "Item A");
+  final quantityA = TextEditingController();
+  final weightA = TextEditingController();
+  final priceA = TextEditingController();
+  final discountA = TextEditingController();
+
+  final nameB = TextEditingController(text: "Item B");
+  final quantityB = TextEditingController();
+  final weightB = TextEditingController();
+  final priceB = TextEditingController();
+  final discountB = TextEditingController();
+
+  @override
+  void dispose() {
+    nameA.dispose();
+    quantityA.dispose();
+    priceA.dispose();
+    discountA.dispose();
+    weightA.dispose();
+
+    nameB.dispose();
+    quantityB.dispose();
+    priceB.dispose();
+    discountB.dispose();
+    weightB.dispose();
+
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            new Text(
-              'You have pushed the button this many times:',
-            ),
-            new Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    return Form(
+        key: _formKey,
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                  controller: nameA,
+                  decoration: InputDecoration(hintText: "Item A name"),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter name for Item A';
+                    }
+                  }),
+              TextFormField(
+                  decoration: InputDecoration(hintText: "Item A weight"),
+                  controller: weightA,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter weight for Item A';
+                    }
+                  }),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Item A quantity"),
+                controller: quantityA,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter quantity for Item A';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Item A price"),
+                controller: priceA,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter price for Item A';
+                  }
+                },
+              ),
+              TextFormField(
+                  decoration: InputDecoration(hintText: "Item A discount"),
+                  controller: discountA,
+                  keyboardType: TextInputType.number),
+              TextFormField(
+                  controller: nameB,
+                  decoration: InputDecoration(hintText: "Item B name"),
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter name for Item B';
+                    }
+                  }),
+              TextFormField(
+                  decoration: InputDecoration(hintText: "Item B weight"),
+                  controller: weightB,
+                  keyboardType: TextInputType.number,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return 'Please enter weight for Item B';
+                    }
+                  }),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Item B quantity"),
+                controller: quantityB,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter quantity for Item B';
+                  }
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(hintText: "Item B price"),
+                controller: priceB,
+                keyboardType: TextInputType.number,
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please enter price for Item B';
+                  }
+                },
+              ),
+              TextFormField(
+                  decoration: InputDecoration(hintText: "Item B discount"),
+                  controller: discountB,
+                  keyboardType: TextInputType.number),
+              Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 16.0),
+                  child: RaisedButton(
+                    onPressed: () {
+                      if (_formKey.currentState.validate()) {
+                        double _quanA = double.parse(quantityA.text);
+                        double _weightA = double.parse(weightA.text);
+                        double _discountA = 100.0;
+                        if (discountA.text != null) {
+                          _discountA -= double.parse(discountA.text);
+                        }
+                        _discountA /= 100.0;
+                        double _priceA = double.parse(priceA.text) * _discountA;
+
+                        double _quanB = double.parse(quantityB.text);
+                        double _weightB = double.parse(weightB.text);
+                        double _discountB = 100.0;
+                        if (discountB.text != null) {
+                          _discountB -= double.parse(discountB.text);
+                        }
+                        _discountB /= 100.0;
+                        double _priceB = double.parse(priceB.text) * _discountB;
+
+                        String answer = "Choose item: ";
+                        if (_priceA < _priceB) {
+                          answer += nameA.text;
+                        } else {
+                          answer += nameB.text;
+                        }
+
+                        Scaffold.of(context)
+                            .showSnackBar(SnackBar(content: Text(answer)));
+                      }
+                    },
+                    child: Text('Submit'),
+                  ))
+            ]));
   }
 }
